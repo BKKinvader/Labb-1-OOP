c336c5dcc782ae8a8c902dc4f1a6169398da0a0e
Tim Nilsson <tim.nilsson97@hotmail.com>
2022-10-20 17:58:03 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb_1_OOP
+{
+    internal class Circle
+    {
+        // Constructor = Parameter och metod
+        float _pi = 3.141f;
+        int _Radius;
+        int _Height;
+        //Parameter samma namn som class
+        public Circle(int Radie,int Height) // Radie är data skickat från Main 
+        {
+            this._Radius = Radie; // Ge _Radius ett nytt värde drån Main
+            this._Height = Height;
+        }
+
+        //metod på hur man räknar ut area
+        public void getArea()
+        {
+            Console.WriteLine(_Radius * _Radius * _pi);
+
+        }
+
+        public void getcircumference()
+        {
+            Console.WriteLine(_Radius + _Radius * _pi);
+
+        }
+
+        public void getvolume()
+        {
+            Console.WriteLine(_Radius * _Radius * _pi * _Height);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Labb_1_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cirkel 1");
+            Circle c1 = new Circle(5,5); // Skickar 5 till class Circle
+            Console.WriteLine("Area");
+            c1.getArea();// Executing c1 genom att kalla på metoden från klassen Circle
+            Console.WriteLine("Omkrets");
+            c1.getcircumference();
+            Console.WriteLine("Volym");
+            c1.getvolume();
+
+            Console.WriteLine("----------------&&---------------");
+
+            Console.WriteLine("Cirkel 2");
+            Circle c2 = new Circle(6,5);// Skickar 6 till class Circle
+            Console.WriteLine("Area");
+            c2.getArea();// Executing c2 genom att kalla på metoden från klassen Circle
+            Console.WriteLine("Omkrets");
+            c2.getcircumference();
+            Console.WriteLine("Volym");
+            c2.getvolume();
+
+
+
+        }
+    }
+}
